e367683850c1dad2b7033a2b8a24ba846530a310
ajayjohnthattil <ajayjohnthattil@gmail.com>
2022-05-27 17:58:18 +0530

    display github repo info sorted by size

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 
+
+
+
+
+
+
+
 function App() {
+
+  const [repos, setRepos] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const [username, setUsername] = useState()
+
+ // username = document.getElementById("username").value;
+
+
+  useEffect(() =>{
+
+    var submit = document.getElementById("subButton");
+       if(username){
+         submit.disabled = false;
+      }
+      else{
+        submit.disabled = true;
+      }
+  },[username]);
+
+  const fetchData = () => {
+    // var username = document.getElementById("username").value;
+    console.log(username)
+    fetch(`https://api.github.com/users/${username}/repos`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          result.sort((a, b) => b.size - a.size)
+          setRepos(result)
+          setError(null)
+          console.log(result)
+          console.log(repos)
+        })
+        .catch(error=>setError(error))
+        
+        
+        //,
+
+        // (error) => {
+        //   setError(error)
+        //   console.log(error);
+        //   });
+        }
+
   return (
     <div className="App">
       <div className="input">
         <label htmlFor="username">Github username: </label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={event => setUsername(event.target.value)} />
         <label htmlFor="fork">Include forks: </label>
         <input id="fork" type="checkbox" />
-        <button>Submit</button>
+        <button id="subButton"  onClick={fetchData}>Submit</button>
       </div>
       <section>
         <header>
@@ -18,14 +70,22 @@ function App() {
           <div className="col">Description</div>
           <div className="col">Size</div>
         </header>
-        <div>
-          <div className="col">C1</div>
-          <div className="col">C2</div>
-          <div className="col">C3</div>
-          <div className="col">C4</div>
-        </div>
+        
+       
+          {!error?  repos.map(item => (
+            <li className="row" key={item.id}>
+              <div className="col">{item.name}</div>
+              <div className="col">{item.language}</div>
+              <div className="col">{item.description}</div>
+              <div id='size' className="col">{item.size}</div>
+             
+            </li>
+          )):<div className="error">Not Found</div>
+          }
+        
+        
       </section>
-      <div className="error">Dummy error</div>
+      
     </div>
   );
 }
